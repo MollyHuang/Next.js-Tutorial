d8be7c00677f1aaba3e2302829f41372eccb9cd1
Molly Huang <molly.h@gmail.com>
2021-03-21 11:56:03 +0800

    #8 - Redirecting Users in 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,19 @@
 // sfc + tab
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    // console.log("useEffect run once")
+    setTimeout(() => {
+      // router.go(1)
+      router.push('/')
+    }, 3000)
+  }, [])
+
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
