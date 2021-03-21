8ff0bdb9dbb1262704b4aa494ec32d3d17d529d7
Molly Huang <molly.h@gmail.com>
2021-03-21 12:53:07 +0800

    #10 - Fetching Data (getStaticProps) - mollyListing/index.js

--- pages/mollyListing/index.js
+++ pages/mollyListing/index.js
@@ -3,7 +3,18 @@
 import Head from 'next/head'
 import styles from '../../styles/MollyListing.module.css'
 
-const MollyListing = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      mollys: data
+    }
+  }
+}
+
+const MollyListing = ({ mollys }) => {
   return (
     <>
       <Head>
@@ -12,6 +23,13 @@ const MollyListing = () => {
       </Head >
       <div>
         <h1>All Mollys</h1>
+        {mollys.map(user => (
+          <div className={styles.single} key={user.id}>
+            <a>
+              <h3>{user.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
