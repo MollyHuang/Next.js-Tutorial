e307a5404ddcdc37b08eb5278cd21fe9962c3eac
Molly Huang <molly.h@gmail.com>
2021-03-21 14:13:27 +0800

    #13 - Fetching a Single Item

--- pages/mollyListing/[id].js
+++ pages/mollyListing/[id].js
@@ -11,14 +11,29 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fullback: false
+    fallback: false
   }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: {
+      user: data
+    }
+  }
+}
+
+const Details = ({ user }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.website}</p>
+      <p>{user.address.city}</p>
     </div>
   );
 }
