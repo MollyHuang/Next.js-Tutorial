bd4d152cd7764c7dc80707f8ae6b9d9dfe5258d8
Molly Huang <molly.h@gmail.com>
2021-03-21 13:39:22 +0800

    #12 - Dynamic Routes - getStaticPaths in mollyListing/[id].js

--- pages/mollyListing/[id].js
+++ pages/mollyListing/[id].js
@@ -1,4 +1,20 @@
 //sfc + tab
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map(user => {
+    return {
+      params: { id: user.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fullback: false
+  }
+}
+
 const Details = () => {
   return (
     <div>
